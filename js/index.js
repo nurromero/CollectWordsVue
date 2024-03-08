37118d668b6fdec2271d11f296e0eadfe1d7f680
Nuriye <nuriyeerdogan38@gmail.com>
2024-03-08 15:26:23 +0100

    works now!

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 Vue.createApp({
   data(){
-    return{
+    return {
       word: '',
+      words: [],
+      message: null,
+    }
+  }, 
+  computed: { //Computed property
+    wordCount() {
+      return this.words.length;
     }
   },
   methods: {
+    saveWord(word){
+      this.words.push(word)
+    },
+    showWord(){
+      if(this.words.length > 0){
+        this.message = this.words.toString()
+      }
+    },
+    clearWord(){
+      this.words = []
+      this.message = '';
+    },
+    
 
   }
 }).mount('#app')
